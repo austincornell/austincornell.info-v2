bf52722051f0c8bdcac3adb7209ad1f1e8a2a589
Austin Cornell <72640569+austincornell@users.noreply.github.com>
2022-09-14 14:28:40 -0400

    changes to white

--- src/Components/HomeBody.js
+++ src/Components/HomeBody.js
@@ -6,7 +6,7 @@ import { useInView } from "react-intersection-observer";
 import { useEffect } from "react";
 import HomeCard from "../Components/HomeCard";
 import Grid from "@mui/material/Grid";
-import AustinRed from "../images/austinRed.jpg";
+import AustinRed from "../images/austinCornell-1.jpg";
 
 
 const containerVariants = {
@@ -56,85 +56,64 @@ function HomeBodySection1(){
     <Box
       sx={{
         height: "50vh",
-        color: "white",
         marginTop: "200px",
       }}
       ref={ref}
       variants={containerVariants}
       initial="hidden"
-    
     >
-      <img className="homeRedImage" src={AustinRed} alt="Austin Red" />
-      <Typography variant="h5">Oh, hello there. My name is</Typography>
-      <Typography
-        variant="h1"
-        sx={{
-          fontWeight: "bold",
-          color: "var(--mainRed)",
-        }}
-      >
-        Austin Cornell
-      </Typography>
-      <Typography variant="body1">
-        I'm super glad you decided to visit. This website was coded by me (with
-        help from various frameworks) as a side-project. I truly hope you can
-        get some information out of it. The goal is to provide anyone with more
-        detailed information about myself while also demonstrating some skills.
-        To start, you can see a quick-bytes section below that should get you my
-        most basic details. If you want more, click around! Don't forget you can
-        email me by clicking button in the top right corner. Happy clicking!
-      </Typography>
-    </Box>
-  );
-}
-
-function HomeBodySection2() {
-
-  const [ref, inView] = useInView();
-
-
-
-  return (
-    <Box
-      sx={{
-        height: "50vh",
-        color: "white",
-        marginTop: "400px",
-      }}
-      ref={ref}
-      variants={containerVariants}
-      initial="hidden"
-      
-    >
-      <Typography
-        variant="h1"
-        sx={{
-          fontWeight: "bold",
-          color: "var(--mainRed)",
-        }}
-      >
-        This is a test 2
-      </Typography>
-      <Typography variant="body1">
-        I am so glad you decided to check this page out
-      </Typography>
-
-      <Grid container spacing={3}>
-        <Grid item xs={12} md={6} lg={3}>
-          <HomeCard />
+      <Grid container spacing={2}>
+        <Grid item md={12} lg={4}>
+          <img className="homeRedImage" src={AustinRed} alt="Austin Red" />
         </Grid>
-        <Grid item xs={12} md={6} lg={3}>
-          <HomeCard />
-        </Grid>
-        <Grid item xs={12} md={6} lg={3}>
-          <HomeCard />
-        </Grid>
-        <Grid item xs={12} md={6} lg={3}>
-          <HomeCard />
+        <Grid item md={12} lg={8}>
+          <Typography variant="h5">Oh, hello there. My name is</Typography>
+          <Typography
+            variant="h1"
+            sx={{
+              fontWeight: "bold",
+              color: "var(--mainRed)",
+            }}
+          >
+            Austin Cornell
+          </Typography>
+          <Typography variant="body1">
+            <a class="hvr-sweep-to-top" href="">
+              I'm super glad
+            </a>{" "}
+            you decided to visit. This website was coded by me (with help from
+            various frameworks) as a side-project. I truly hope you can get some
+            information out of it. The goal is to provide anyone with more
+            detailed information about myself while also demonstrating some
+            skills. To start, you can see a quick-bytes section below that
+            should get you my most basic details. If you want more, click
+            around! Don't forget you can email me by clicking button in the top
+            right corner. Happy clicking!
+          </Typography>
         </Grid>
       </Grid>
     </Box>
   );
 }
+
+function HomeBodySection2(){
+  return (
+    <Grid container spacing={3}>
+      <Grid item sm={12} md={6}>
+        <HomeCard className=""></HomeCard>
+      </Grid>
+      <Grid item sm={12} md={6}>
+        <HomeCard className=""></HomeCard>
+      </Grid>
+      <Grid item sm={12} md={6}>
+        <HomeCard className=""></HomeCard>
+      </Grid>
+      <Grid item sm={12} md={6}>
+        <HomeCard className=""></HomeCard>
+      </Grid>
+    </Grid>
+  );
+}
+
 
 export default HomeBody;
--- src/Components/NavBarItem.js
+++ src/Components/NavBarItem.js
@@ -2,13 +2,7 @@ import React from "react";
 import Box from "@mui/system/Box";
 import Button from '@mui/material/Button';
 import "../App.css";
-import { ThemeProvider, styled, createTheme } from "@mui/material/styles";
 import { Link } from "react-router-dom";
-// borderStyle: "none",
-//             "&:hover": {
-//               borderColor: "var(--mainRed)",
-//               color: "var(--mainRed)",
-//             },
 
 
 function NavBarItem(props) {
