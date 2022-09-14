4c71453e6149befc6ad92999b96f025b3b0279a2
Austin Cornell <72640569+austincornell@users.noreply.github.com>
2022-09-14 14:28:31 -0400

    updates to white home page

--- src/Components/HomeCard.js
+++ src/Components/HomeCard.js
@@ -2,6 +2,7 @@ import React from "react";
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
+import CardMedia from "@mui/material/CardMedia";
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import { ThemeProvider, createTheme } from "@mui/material/styles";
@@ -14,36 +15,29 @@ const darkTheme = createTheme({
 
 function HomeCard() {
     return (
-      <ThemeProvider theme={darkTheme}>
-        <Card
-          sx={{ minWidth: 275 }}
-          whileHover= {{ scale: 1.05,}}
-        >
-          <CardContent>
-            <Typography
-              sx={{ fontSize: 14 }}
-              color="text.secondary"
-              gutterBottom
-            >
-              Word of the Day
-            </Typography>
-            <Typography variant="h5" component="div">
-              benevolent
-            </Typography>
-            <Typography sx={{ mb: 1.5 }} color="text.secondary">
-              adjective
-            </Typography>
-            <Typography variant="body2">
-              well meaning and kindly.
-              <br />
-              {'"a benevolent smile"'}
-            </Typography>
-          </CardContent>
-          <CardActions>
-            <Button size="small">Learn More</Button>
-          </CardActions>
-        </Card>
-      </ThemeProvider>
+      
+    <Card sx={{ maxWidth: 700 }}
+    variant="outlined">
+      <CardMedia
+        component="img"
+        alt="green iguana"
+        height="140"
+        image=""
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Lizard
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Share</Button>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
     );
 
 }
